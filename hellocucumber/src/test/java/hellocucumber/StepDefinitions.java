fab77504fc340b0316da4175e3d1080fad821396
Damien_Laboute <125353264+DamienZoldyck@users.noreply.github.com>
2025-01-07 10:36:09 +0100

    Ajout 1er test IsItFriday

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -2,9 +2,13 @@ package hellocucumber;
 
 import io.cucumber.java.en.*;
 
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class StepDefinitions {
+    
+    private String today;
+    private String actualAnswer;
 
     @Given("an example scenario")
     public void anExampleScenario() {
@@ -18,4 +22,24 @@ public class StepDefinitions {
     public void theScenarioPasses() {
     }
 
-}
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask if it's Friday")
+    public void i_ask_whether_it_s_friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("the answer should be \"No\"")
+    public void i_should_be_told() {
+        assertEquals("No", actualAnswer);
+        }
+
+    }
+    class IsItFriday {
+        static String isItFriday(String today) {
+            return "Friday".equals(today) ? "Yes" : "No";
+        }
+    }
